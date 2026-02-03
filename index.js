9fc26da4f64c3934cfa3e9ff8ff9230826bea5d3
Somya Upadhyay <39206850+somyaup@users.noreply.github.com>
2026-02-04 00:34:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const startBtn = document.getElementById("startBtn");
+const nameInput = document.getElementById("playerName");
+
+function getOrCreatePlayerNumber() {
+  let num = localStorage.getItem("playerNumber");
+  if (!num) {
+    num = Math.floor(100000 + Math.random() * 900000); // 6-digit
+    localStorage.setItem("playerNumber", num);
+  }
+  return num;
+}
+
+startBtn.addEventListener("click", () => {
+  const name = nameInput.value.trim();
+
+  if (!name) {
+    alert("Please enter your name");
+    return;
+  }
+
+  const playerNum = getOrCreatePlayerNumber();
+  const playerNumber = String(playerNum);
+  localStorage.setItem("playerName", name);
+  localStorage.setItem("playerNumber", playerNumber);
+  window.location.href = "game.html";
+});
+
---
+++ indexT1.js
@@ -0,0 +1,28 @@
+const startBtn = document.getElementById("startBtn");
+const nameInput = document.getElementById("playerName");
+
+function getOrCreatePlayerNumber() {
+  let num = localStorage.getItem("playerNumber");
+  if (!num) {
+    num = Math.floor(100000 + Math.random() * 900000); // 6-digit
+    localStorage.setItem("playerNumber", num);
+  }
+  return num;
+}
+
+startBtn.addEventListener("click", () => {
+  const name = nameInput.value.trim();
+
+  if (!name) {
+    alert("Please enter your name");
+    return;
+  }
+
+  const playerNum = getOrCreatePlayerNumber();
+  const playerNumber = String(playerNum);
+  localStorage.setItem("playerName", name);
+  localStorage.setItem("playerNumber", playerNumber);
+  window.location.href = "gameT1.html";
+});
+
+
