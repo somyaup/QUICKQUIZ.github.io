5d59a14c00842436557e38789d74622df296a2a1
Somya Upadhyay <39206850+somyaup@users.noreply.github.com>
2026-02-04 18:43:24 +0530

    Update gameT1.js
    time changed to 450 seconds

--- gameT1.js
+++ gameT1.js
@@ -21,7 +21,7 @@ let gameLog = {
 let setStartTimestamp = null;
 const CORRECT_BONUS = 10;
 
-const TOTAL_TIME = 900; // seconds
+const TOTAL_TIME = 450; // seconds
 let timeRemaining = TOTAL_TIME;
 let timerInterval = null;
 
