9db72b78977faa403942aa50635e103248d68a6e
Somya Upadhyay <39206850+somyaup@users.noreply.github.com>
2026-02-05 14:12:04 +0530

    Update gameT1B.js

--- gameT1B.js
+++ gameT1B.js
@@ -88,8 +88,8 @@ function loadSet() {
   playerDisplay.innerText = `${playerName} (#${playerNumber})`;
   setTitle.innerText = currentSet.set;
   // Progress / UI
-  progressText.innerText =
-    `${currentSet.set} (${currentSetIndex + 1} / ${sets.length})`;
+  // progressText.innerText =
+  //   `${currentSet.set} (${currentSetIndex + 1} / ${sets.length})`;
   progressBarFull.style.width =
     `${((currentSetIndex + 1) / sets.length) * 100}%`;
 
